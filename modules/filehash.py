cd4338c2ee8ec1b2f987180d776db4546d20005f
navent <18219017@std.stei.itb.ac.id>
2022-04-16 13:34:12 +0700

    add sha-1 function

--- modules/filehash.py
+++ modules/filehash.py
@@ -1,1 +1,12 @@
 #
+from hashlib import sha1
+
+def baseHash(message: bytes)->bytes:
+    return sha1(message).digest()
+
+def hashFile(filedir: str)->bytes:
+    f = open(f"{filedir}","rb")
+    fileInside = f.read()
+    f.close()
+
+    return baseHash(fileInside)
